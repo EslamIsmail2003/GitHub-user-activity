84af0505dec78e899c789ba64403ac6593776f27
eslam <eslam.ismail2003@gmail.com>
2026-05-10 21:32:48 +0200

    user enters github username. fetch username from api sendAsync method and build the response in real time

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your github username: ");
+        String username = scanner.next();
+        String url = "https://api.github.com/users/" + username;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).header("Accept","application/json").build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println).join();
     }
 }
